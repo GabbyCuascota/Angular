e057f62cfe70212c311f49a4e045b6462e60d6cb
GabbyCuascota <gabriela.cuascota@hotmail.com>
2018-05-11 18:02:10 -0500

    peliculas

--- SpoilerApp6/src/app/peliculas/pelicula-banner/pelicula-banner.component.ts
+++ SpoilerApp6/src/app/peliculas/pelicula-banner/pelicula-banner.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pelicula-banner.component.css']
 })
 export class PeliculaBannerComponent implements OnInit {
-
-  constructor() { }
+  urlImagen = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL_FOh_xn19xgXf_Uq5OK3WWzAmoVoZLHCO2vjFd4LKaxijU59eg';
+  descripcionImagen = 'Imagen de la pelicula Tomb Raider';
+  nombrePelicula = 'Tomb Raider';
+  descripcionPelicula = 'Alicia vikander,Walton Googins';
+  esEstreno = false;
+  textoEstreno:string;
+  claseEstreno:string;
+  constructor() {}
 
   ngOnInit() {
+    if (this.esEstreno){
+      this.textoEstreno = 'Estreno';
+      this.claseEstreno = 'sa-color-estado-amarillo'
+    }else{
+      this.textoEstreno = 'Proximamente';
+      this.claseEstreno = 'sa-color-estado-amarillo';
+    }
   }
 
 }
